d8a2721aca1ebd1064a628a94abbd335293d9720
Kazi Toufiq Wadud <kazitoufiq@users.noreply.github.com>
2021-04-24 13:49:54 +1000

    Create apply_func_to_each_row.py

---
+++ apply_func_to_each_row.py
@@ -0,0 +1,11 @@
+def build_time_features(vector):
+    pickup_datetime = vector[0]
+    month_num = pickup_datetime.month
+    day_of_month = pickup_datetime.day
+    day_of_week = pickup_datetime.weekday()
+    hour_of_day = pickup_datetime.hour
+    
+    return pd.Series((month_num, day_of_month, day_of_week, hour_of_day))
+
+green_taxi_df[["month_num", "day_of_month","day_of_week", "hour_of_day"]] = green_taxi_df[["lpepPickupDatetime"]].apply(build_time_features, axis=1)
+green_taxi_df.head(10)
